060c06f6ba5a749faf4b8873f146156abcea2d9d
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-14 14:16:51 +0400

    Added global styles

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { globalStyle } from './src/assets';
 import { Home } from './src/screens';
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <SafeAreaView>
+      <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
         <Home />
       </SafeAreaView>
     </NavigationContainer>
---
+++ src/assets/styles/globalStyle.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyle = StyleSheet.create({
+  backgroundWhite: {
+    backgroundColor: '#FFFFFF',
+  },
+  flex: {
+    flex: 1,
+  },
+});
