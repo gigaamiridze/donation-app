591ef482edfdfbb45ef5a54cba34e3c75d85eb5c
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-15 21:45:22 +0400

    Defined badge width

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { View, Text } from 'react-native';
 import { IBadgeProps } from '../../interfaces';
 import { style } from './style';
 
 function Badge({ title }: IBadgeProps) {
+  const [width, setWidth] = useState<number>(0);
+  const textRef = useRef<Text>(null);
+  const paddingHorizontal = 10;
+  const badgeWidth = {
+    width: paddingHorizontal * 2 + width,
+  };
+
   return (
-    <View style={style.badge}>
-      <Text style={style.title}>{title}</Text>
+    <View style={[style.badge, badgeWidth]}>
+      <Text 
+        ref={textRef}
+        onTextLayout={(event) => {
+          setWidth(event.nativeEvent.lines[0].width);
+        }}
+        style={style.title}
+      >
+        {title}
+      </Text>
     </View>
   )
 }
