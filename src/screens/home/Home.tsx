8b14b2ed6269aec6f795417328030d96dae88c42
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-16 13:15:23 +0400

    Added keyboard dismiss functionality

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { globalStyle } from '../../assets';
 import { Header, Button, Tab, Badge, Search } from '../../components';
 
 function Home() {
   return (
-    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <Header title='Azzahri A.' type={1} />
-      <Button title='Donate' />
-      <Button title='Confirm Payment' isDisabled={true} />
-      <Tab title='Highlight' />
-      <Tab title='Lifestyle' isInactive={true} />
-      <Badge title='Environment' />
-      <Search />
-    </SafeAreaView>
+    <TouchableWithoutFeedback
+      onPress={Keyboard.dismiss}
+      accessible={false}
+    >
+      <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+        <Header title='Azzahri A.' type={1} />
+        <Button title='Donate' />
+        <Button title='Confirm Payment' isDisabled={true} />
+        <Tab title='Highlight' />
+        <Tab title='Lifestyle' isInactive={true} />
+        <Badge title='Environment' />
+        <Search />
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   )
 }
 
