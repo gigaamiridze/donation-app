0b3b25b2d22bfe212ba0090a3d420248236a756d
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-16 13:10:26 +0400

    Added handle focus function on Pressable component for TextInput component focus

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Pressable, TextInput, Image } from 'react-native';
 import { style } from './style';
 
 function Search() {
+  const textInputRef = useRef<TextInput>(null);
+
+  const handleFocus = () => {
+    textInputRef.current?.focus();
+  }
+
   return (
-    <Pressable style={style.searchInputContainer}>
+    <Pressable 
+      style={style.searchInputContainer}
+      onPress={handleFocus}
+    >
       <Image source={require('../../assets/images/search-icon.png')} />
       <TextInput 
+        ref={textInputRef}
         placeholder='Search'
         style={style.searchInput}
       />
