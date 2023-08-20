8b54347df64b68e4ef415c0e171f7dd95b27e4e6
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-20 11:34:36 +0400

    Added input fields for registration screen

--- src/screens/Registration/Registration.tsx
+++ src/screens/Registration/Registration.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView, ScrollView, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { BackButton, Header } from '../../components';
+import { BackButton, Header, Input } from '../../components';
 import { globalStyle } from '../../assets';
 import { style } from './style';
 
 function Registration() {
+  const [fullName, setFullName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
   const navigation = useNavigation();
 
   return (
@@ -16,6 +19,23 @@ function Registration() {
         contentContainerStyle={style.container}
       >
         <Header title='Hello and Welcome!' type={1} />
+        <Input 
+          label='First & Last Name'
+          placeholder='Enter your full name...'
+          onChangeText={value => setFullName(value)}
+        />
+        <Input 
+          label='Email'
+          placeholder='Enter your email...'
+          keyboardType='email-address'
+          onChangeText={value => setEmail(value)}
+        />
+        <Input 
+          label='Password'
+          placeholder='******'
+          secureTextEntry={true}
+          onChangeText={value => setPassword(value)}
+        />
       </ScrollView>
     </SafeAreaView>
   )
