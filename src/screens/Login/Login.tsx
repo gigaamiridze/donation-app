e478be2a575569850ec1c23ca30f92d05e92805d
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-20 10:24:04 +0400

    Added input field for password

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
-import { SafeAreaView, ScrollView, Text } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native';
 import { globalStyle } from '../../assets';
-import { Input } from '../../components';
+import { Header, Input } from '../../components';
+import { style } from './style';
 
 function Login() {
   const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
   return (
     <SafeAreaView style={globalStyle.screenContainer}>
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView 
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={style.container}
+      >
+        <Header title='Welcome Back' type={1} />
         <Input 
           label='Email'
           placeholder='Enter your email...'
           keyboardType='email-address'
           onChangeText={value => setEmail(value)}
+        />
+        <Input 
+          label='Password'
+          placeholder='******'
+          secureTextEntry={true}
+          onChangeText={value => setPassword(value)}
         />
       </ScrollView>
     </SafeAreaView>
